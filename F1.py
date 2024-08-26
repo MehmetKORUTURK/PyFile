ee319ea1ad80230e2fe3f0018b02fe1b78e5fcf2
Mehmet KORUTURK <mkoruturk@vt.edu>
2024-08-26 08:43:46 +0300

    Revert "e commit"
    This reverts commit 3e1037ef3de27ffe640dc1f184baeed98acab6d2.

--- F1.py
+++ F1.py
@@ -10,9 +10,12 @@ print("bazi seyler degisti gibi ama anlamadim")
 print("suan az da olsa nasil yapacagimi anladim gibi ama biraz olsun kullanmam gerek")
 
 print("az once geri almayi ogrendim simdi farkli seyler denemek istyorum")
+<<<<<<< HEAD
 
 print("herseyi deniyotum fakat biraz karisik geldi")
 
 print("suan vs code nasil calistigini anladim gibi biraz gec oldu ama sonunda cozdum gibi")
 
 
+=======
+>>>>>>> parent of 3e1037e (e commit)
